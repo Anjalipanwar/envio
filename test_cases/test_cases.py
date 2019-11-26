a01db60c7f13544543cc5ec3a8a5836dba60b959
Anjali <official.anjali@gmail.com>
2019-11-26 15:15:44 +0530

    Update test_cases.py

--- test_cases/test_cases.py
+++ test_cases/test_cases.py
@@ -14,9 +14,29 @@ class TestSuite(unittest.TestCase):
         self.login_obj = UIloginSuite()
 
     @pytest.mark.functional
-    def testLogin(self):
+    def testLogin01(self):
         logger.logger.info("Executing Login test cases")
         self.login_obj.init_browser()
+
+    @pytest.mark.functional
+    def testLogin02(self):
+        logger.logger.info("Executing Login test case 02")
+        self.login_obj.login()
+
+    @pytest.mark.functional
+    def testLogin03(self):
+        logger.logger.info("Executing Login test case 02")
+        self.login_obj.settings()
+
+    @pytest.mark.functional
+    def testLogin04(self):
+        logger.logger.info("Executing Login test case 02")
+        self.login_obj.logout()
+
+    @pytest.mark.functional
+    def testLogin05(self):
+        logger.logger.info("Executing Login test case 02")
+        self.login_obj.shut_driver()
 
     @classmethod
     def tearDownClass(self):
@@ -28,7 +48,11 @@ if __name__ == '__main__':
 
     # Test Cases
     test_obj = unittest.TestSuite()
-    test_obj.addTest(TestSuite('testLogin'))
+    test_obj.addTest(TestSuite('testLogin01'))
+    test_obj.addTest(TestSuite('testLogin02'))
+    test_obj.addTest(TestSuite('testLogin03'))
+    test_obj.addTest(TestSuite('testLogin04'))
+    test_obj.addTest(TestSuite('testLogin05'))
 
     runner = unittest.TextTestRunner()
     runner.run(test_obj)
